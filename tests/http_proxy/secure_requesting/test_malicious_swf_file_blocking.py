0dfba1b07eca203cbeb744e4e7098be9f4bae988
polllaris <polllaris@protonmail.com>
2025-06-12 06:26:31 -0400

    add tests to ensure SWFs that use flash.display.Loader are blocked and add the missing word "block" in first tests

--- tests/http_proxy/secure_requesting/test_malicious_swf_file_blocking.py
+++ tests/http_proxy/secure_requesting/test_malicious_swf_file_blocking.py
@@ -3,15 +3,25 @@ from . import SecureRequestingUseCaseTestFixture
 
 class TestBlockingOfFlashFilesystemFile(SecureRequestingUseCaseTestFixture):
 
-	async def test_should_no_compression_variants_of_swf_files_which_use_flash_filesystem_file(self):
+	async def test_should_block_no_compression_variants_of_swf_files_which_use_flash_filesystem_file(self):
 		await self._verifyUseCaseWillBlockResponseWithSwfSampleFile("FlashFilesystemFile-fws.swf")
 
-	async def test_should_zlib_compression_variants_of_swf_files_which_use_flash_filesystem_file(self):
+	async def test_should_block_zlib_compression_variants_of_swf_files_which_use_flash_filesystem_file(self):
 		await self._verifyUseCaseWillBlockResponseWithSwfSampleFile("FlashFilesystemFile-cws.swf")
 
-
-	async def test_should_lzma_compression_variants_of_swf_files_which_use_flash_filesystem_file(self):
+	async def test_should_block_lzma_compression_variants_of_swf_files_which_use_flash_filesystem_file(self):
 		await self._verifyUseCaseWillBlockResponseWithSwfSampleFile("FlashFilesystemFile-zws.swf")
+
+
+class TestBlockingOfFlashDisplayLoader(SecureRequestingUseCaseTestFixture):
+	async def test_should_block_no_compression_variants_of_swf_files_which_use_flash_display_loader(self):
+		await self._verifyUseCaseWillBlockResponseWithSwfSampleFile("FlashDisplayLoader-fws.swf")
+
+	async def test_should_block_zlib_compression_variants_of_swf_files_which_use_flash_display_loader(self):
+		await self._verifyUseCaseWillBlockResponseWithSwfSampleFile("FlashDisplayLoader-cws.swf")
+
+	async def test_should_block_lzma_compression_variants_of_swf_files_which_use_flash_filesystem_file(self):
+		await self._verifyUseCaseWillBlockResponseWithSwfSampleFile("FlashDisplayLoader-zws.swf")
 
 
 class TestNoneBlockingOfNonMaliciousSWFFiles(SecureRequestingUseCaseTestFixture):
